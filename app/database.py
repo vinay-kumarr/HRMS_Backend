a1b1c491a880e220f9a29c7992c84ba7ea2e1517
vinay-kumarr <boudhvinay883@gmail.com>
2026-01-29 13:52:48 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -11,7 +11,7 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
 DB_NAME = os.getenv("DB_NAME", "hrms_lite")
 
-# Standard configuration
+
 client = AsyncIOMotorClient(MONGO_URI, tlsCAFile=certifi.where())
 db = client[DB_NAME]
 
